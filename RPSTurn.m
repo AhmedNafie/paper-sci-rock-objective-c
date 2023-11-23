18dc2c9558fea2c72c04484a52c5c8feda81bc59
AhmedNafie <ahmedtareknafie@gmail.com>
2023-11-23 16:38:39 +0200

    Add logic to defeats method

--- RPSTurn.m
+++ RPSTurn.m
@@ -51,9 +51,15 @@
     return Rock;
 }
 
--(BOOL)defeats:(RPSTurn*) turn {
-    return false;
-    
+-(BOOL)defeats:(RPSTurn*) opponent {
+    if ((self.move == Paper && opponent.move == Rock) ||
+        (self.move == Scissors && opponent.move == Paper) ||
+        (self.move == Rock && opponent.move == Scissors ))
+        {
+            return true;
+        } else {
+            return false;
+        }
 }
 
 
