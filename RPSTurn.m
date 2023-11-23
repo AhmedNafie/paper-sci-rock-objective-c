b8af06bbd170357e4cebbfb561faf8cadc66fa1a
AhmedNafie <ahmedtareknafie@gmail.com>
2023-11-23 16:11:08 +0200

    add logic to generateMove

--- RPSTurn.m
+++ RPSTurn.m
@@ -10,24 +10,38 @@
 @implementation RPSTurn
 
 -(instancetype)initWithMove:(Move) move {
-
+    
     self = [super init];
-
+    
     if (self){
         _move = move;
     }
-
+    
     return self;
 }
 
 -(Move)generateMove  {
+    
+    NSUInteger randomNumber = arc4random_uniform(3);
+    switch(randomNumber) {
+        case 0:
+            return Rock;
+            break;
+        case 1:
+            return Paper;
+            break;
+        case 2: return Scissors;
+            break;
+        default:
+            return Invalid;
+            break;
+    }
     return Rock;
 }
 
-
 -(BOOL)defeats:(RPSTurn*) turn {
     return false;
-
+    
 }
 
 
